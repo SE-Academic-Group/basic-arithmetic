ccc3be08f545451fa88f4985b6f4cc01de2d751f
Truong Minh Phat <tmphat1312@gmail.com>
2024-10-22 13:25:18 +0000

    chore: setup tailwindcss and prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        input: {
+          DEFAULT: "#424c52",
+          foreground: "#ffffff",
+        },
+        operator: {
+          DEFAULT: "#f5913e",
+        },
+        number: {
+          DEFAULT: "#e0e0e0",
+        },
+        background: {
+          DEFAULT: "#ffffff",
+        },
+        foreground: {
+          DEFAULT: "#000000",
+        },
+      },
+    },
   },
   plugins: [],
 };
