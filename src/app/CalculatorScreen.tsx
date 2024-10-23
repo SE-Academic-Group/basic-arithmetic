fa2ab5da1fb33577a6f562ae19af3e4561a6340b
Truong Minh Phat <tmphat1312@gmail.com>
2024-10-23 08:25:23 +0000

    test: test the app components

--- src/app/CalculatorScreen.tsx
+++ src/app/CalculatorScreen.tsx
@@ -6,8 +6,8 @@ type CalculatorScreenProps = {
 export function CalculatorScreen({ input, result }: CalculatorScreenProps) {
   return (
     <div className="whitespace-normal break-words bg-input p-3 text-end text-2xl text-input-foreground">
-      <span>{input || 0}</span>
-      <span>{result ? `=${result}` : ""}</span>
+      <span data-testid="input">{input || 0}</span>
+      <span data-testid="result">{result ? `=${result}` : ""}</span>
     </div>
   );
 }
