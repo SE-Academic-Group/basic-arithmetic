1a2ef5e68f99ecbb743a0c1255c7bc89ff78eeb8
Truong Minh Phat <tmphat1312@gmail.com>
2024-10-23 03:21:41 +0000

    test: test the isNumber

--- src/utils/isNumber.test.ts
+++ src/utils/isNumber.test.ts
@@ -1,8 +1,35 @@
-import { describe } from "vitest";
 import { isNumber } from "./isNumber";
 
 describe("isNumber", () => {
-  it("should return true if the value is a number", () => {
-    expect(isNumber("1")).toBe(true);
+  it("should return true if the value is a integer number", () => {
+    const numbers = Array.from({ length: 10 }, (_, i) => i);
+
+    numbers.forEach((number) => {
+      expect(isNumber(number.toString())).toBe(true);
+    });
+  });
+
+  it("should return true if the value is a float number", () => {
+    const numbers = Array.from({ length: 10 }, (_, i) => i);
+
+    numbers.forEach((number) => {
+      expect(isNumber(`${number}.5`)).toBe(true);
+    });
+  });
+
+  it("should return false if the value is an operator", () => {
+    const operators = ["+", "-", "*", "/"];
+
+    operators.forEach((operator) => {
+      expect(isNumber(operator)).toBe(false);
+    });
+  });
+
+  it("should return false if the value is a calculation function", () => {
+    const fns = ["AC", "DEL", "=", "Ans"];
+
+    fns.forEach((fn) => {
+      expect(isNumber(fn)).toBe(false);
+    });
   });
 });
